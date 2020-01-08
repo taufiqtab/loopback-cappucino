ac9c2cc60362b988c01b7d279dba1f1ae7d9ff3f
taufiqtab <taufiqtab77@gmail.com>
2020-01-08 17:59:58 +0700

    custom method

--- common/models/users.js
+++ common/models/users.js
@@ -1,15 +1,21 @@
 'use strict';
 
 module.exports = function(Users) {
-    Users.greet = async function(msg, val1, val2) {
-        return 'Greetings... ' + msg + " values = " + (val1 + val2);
+
+    Users.greet = function(msg, val1, val2, ids, cb) {
+        Users.findById( ids, function (err, instance) {
+            var val = "Nama Orang yang kamu cari " + instance.nama;
+            cb(null, 'Greetings... ' + msg + " values = " + (val1 + val2) + " orangnya = " + val);
+        });
     }
 
     Users.remoteMethod('greet', {
+          http: {path: '/greet', verb: 'post'},
           accepts: [
               {arg: 'msg', type: 'string'},
               {arg: 'val1', type: 'number'},
-              {arg: 'val2', type: 'number'}
+              {arg: 'val2', type: 'number'},
+              {arg: 'ids', type: 'number'}
           ],
           returns: {arg: 'greeting', type: 'string'}
     });
